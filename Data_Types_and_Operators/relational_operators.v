5c4e0008e6ae570ab9c0c44665526b007910c454
Cauchy <caotricg1308@gmail.com>
2025-10-01 20:25:20 +0700

    Clean template

--- Data_Types_and_Operators/relational_operators.v
+++ Data_Types_and_Operators/relational_operators.v
@@ -1,20 +1,20 @@
-module relational_operators();  
+module relational_operators();
     reg result; 
     
     // Procedure used to continuously monitor 'a', and 'b'
-	initial begin
+    initial begin
         $monitor("MON result = %1b", result);
-	end
+    end
   
-	// Procedure used to generate stimulus
-	initial begin
+    // Procedure used to generate stimulus
+    initial begin
         #1; result = 3 < 0;
         #1; result = 3 < 6'b00_1111;            // 3 < 15?
         #1; result = 6 > 6;
         #1; result = 4'b1001 <= 4'b1010;        // 9 <= 10?
         #1; result = 4'b100X > 4'b1010;
         #1; result = 99 >= 98;
-	end
+    end
 endmodule
 
 /* OUTPUT
