a2fd7ec9f95720a4c12d79c38cc708e9c44496e1
Cauchy <caotricg1308@gmail.com>
2025-10-04 20:25:43 +0700

    Add Structural Design

---
+++ Structural_Design/comparator_1bit/tb_comparator_1bit.v
@@ -0,0 +1,33 @@
+`timescale 1us/1ns
+module tb_comp();
+    reg a, b;
+    wire smaller, greater, equal;
+
+    // Instantiate the DUT
+    comparator_1bit CMP1(
+        .a(a),
+        .b(b),
+        .smaller(smaller),
+        .equal(equal),
+        .greater(greater)
+    );
+   
+    initial begin
+        $monitor(" a = %b, b = %b, smaller = %b, equal = %b, greater = %b",
+                    a, b, smaller, equal, greater);
+        #1; a = 0; b = 0;
+        #1; a = 1; b = 0;
+        #1; a = 1; b = 0;
+        #1; a = 0; b = 1;
+        #1; a = 1; b = 1;      
+        #1; $stop;
+    end  
+endmodule
+
+/* OUTPUT
+a = x, b = x, smaller = x, equal = x, greater = x
+a = 0, b = 0, smaller = 0, equal = 1, greater = 0
+a = 1, b = 0, smaller = 0, equal = 0, greater = 1
+a = 0, b = 1, smaller = 1, equal = 0, greater = 0
+a = 1, b = 1, smaller = 0, equal = 1, greater = 0
+*/
---
+++ Structural_Design/mux_tristate/tb_mux_tristate.v
@@ -0,0 +1,35 @@
+`timescale 1us/1ns
+
+module tb_mux_tristate();
+    reg a, b;
+    reg sel;
+    wire dout;
+	
+    // Instantiate the DUT
+    mux_tristate MUX1(
+        .a(a),
+        .b(b),
+        .sel(sel),
+        .dout(dout)
+    );
+      
+    // Toggle sel (select line)
+    initial begin
+        $monitor(" a = %b, b = %b, sel = %b, dout = %b",a, b, sel, dout);
+        #1; a = 0; b = 0; sel = 0;
+        #1; a = 1; b = 0; sel = 0;
+        #1; a = 1; b = 0; sel = 1;
+        #1; a = 0; b = 1; sel = 1;
+        #1; a = 0; b = 0; sel = 0;      
+        #1;
+    end
+endmodule
+
+/* Output:
+a = x, b = x, sel = x, dout = x
+a = 0, b = 0, sel = 0, dout = 0
+a = 1, b = 0, sel = 0, dout = 1
+a = 1, b = 0, sel = 1, dout = 0
+a = 0, b = 1, sel = 1, dout = 1
+a = 0, b = 0, sel = 0, dout = 0
+*/
