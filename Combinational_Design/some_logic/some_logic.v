9b111ebd71022b5063b93d5cfacd9257f01dc9a0
Cauchy <caotricg1308@gmail.com>
2025-10-06 16:30:12 +0700

    Add first Combinational Design

---
+++ Combinational_Design/some_logic/some_logic.v
@@ -0,0 +1,15 @@
+module some_logic (
+    input a,
+    input b,
+    output c
+    );
+    
+    // Declare internal nets
+    wire a_not;
+    wire a_or_b;
+    
+    // The order of the assignments in NOT important
+    assign c = a_or_b | (a_or_b & a_not);
+    assign a_not = ~a;
+    assign a_or_b = a | b;
+endmodule
---
+++ Combinational_Design/some_logic/tb_some_logic.v
@@ -0,0 +1,32 @@
+`timescale 1us/1ns
+
+module tb_logic();
+    reg a;
+    reg b;
+    wire c;
+	
+    // Instantiate the DUT
+    some_logic LOGIC(
+        .a(a),
+        .b(b),
+        .c(c)
+    );
+
+    // Toggle a and b
+    initial begin
+        $monitor("a = %b, b = %b, c = %b", a ,b ,c);
+        #1; a = 0; b = 0;
+        #1; a = 1; b = 0;
+        #1; a = 0; b = 1;
+        #1; a = 1; b = 1;
+        #1;
+    end
+endmodule
+
+/* Output
+a = x, b = x, c = x
+a = 0, b = 0, c = 0
+a = 1, b = 0, c = 1
+a = 0, b = 1, c = 1
+a = 1, b = 1, c = 1
+*/
