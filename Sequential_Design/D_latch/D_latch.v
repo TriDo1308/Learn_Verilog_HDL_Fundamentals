1067f78c719b8a3f41fc7fa013a2a5eb6a534e83
Cauchy <caotricg1308@gmail.com>
2025-10-15 20:08:43 +0700

    Add D latch

---
+++ Sequential_Design/D_latch/D_latch.v
@@ -0,0 +1,18 @@
+module D_latch(
+    input d,
+	input enable,
+	output q,
+    output q_not
+    );
+	
+	reg dlatch;
+	
+	// The D-Lach is level sensitive
+	always @(enable or d) begin
+	    if(enable)
+		    dlatch <= d; 
+	end
+	
+	assign q = dlatch;
+	assign q_not = ~q;
+endmodule
