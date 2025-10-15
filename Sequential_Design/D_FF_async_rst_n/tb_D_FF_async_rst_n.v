3d82deb63d03ae33d5cc26f85633dee12abc2666
Cauchy <caotricg1308@gmail.com>
2025-10-15 22:04:16 +0700

    Add D FF async reset n

--- Sequential_Design/D_FF_async_rst_n/tb_D_FF_async_rst_n.v
+++ Sequential_Design/D_FF_async_rst_n/tb_D_FF_async_rst_n.v
@@ -9,37 +9,37 @@ module tb_D_FF_rst_n();
     wire q_not;
 	reg [1:0] delay;
     integer i;
-
+	
 	// Instantiate the DUT
-	D_FF_sync_rst_n DFF0(
+	D_FF_async_rst_n DFF0(
 	    .reset_n(reset_n),
 	    .clk    (clk    ),
         .d      (d      ),
 	    .q      (q      ),
         .q_not  (q_not  )
 	);
-
+	
 	// Create the clk signal
 	always begin
 	    #0.5 clk = ~clk;
 	end
-
+	
     // Create stimulus	  
     initial begin
         reset_n = 0; d = 0;
 		for (i = 0; i < 4; i = i + 1) begin
-		    delay = i + 1;
-		    #(delay) d = ~d;
+		   delay = i + 1;
+		   #(delay) d = ~d;
 		end
-
+		
 		reset_n = 1;
 	    for (i = 0; i < 4; i = i + 1) begin
-		    delay = i + 1;
-		    #(delay) d = ~d;					// toggle the FF at random times	
+		   delay = i + 1;
+		   #(delay) d = ~d;                     // toggle the FF at random times
 		end	
-        #(0.2); reset_n = 0;            		// reset the FF again		
+        #(0.2); reset_n = 0;                    // reset the FF again		
 	end
-
+	
     // This will stop the simulator when the time expires
     initial begin
         #40 $finish;
