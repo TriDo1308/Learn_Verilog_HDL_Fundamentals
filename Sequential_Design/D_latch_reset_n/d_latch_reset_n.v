2f3614c3becad182401cbe295aae9c445eb6df20
Cauchy <caotricg1308@gmail.com>
2025-10-15 21:00:48 +0700

    Add D latch reset n

---
+++ Sequential_Design/D_latch_reset_n/d_latch_reset_n.v
@@ -0,0 +1,21 @@
+module d_latch_rst_n(
+    input d,
+	input enable,
+	input reset_n,
+	output q,
+    output q_not
+    );
+	
+	reg dlatch;
+	
+	// The D-Lach is level sensitive
+	always @(enable or d or reset_n) begin
+	    if (!reset_n)
+		    dlatch <= 1'b0;
+	    else if(enable)
+		    dlatch <= d; 
+	end
+	
+	assign q = dlatch;
+	assign q_not = ~q;
+endmodule
--- Sequential_Design/D_latch_reset_n/tb_d_latch_reset_n.v
+++ Sequential_Design/D_latch_reset_n/tb_d_latch_reset_n.v
@@ -1,16 +1,18 @@
 `timescale 1us/1ns
 
-module tb_D_latch();
+module tb_d_latch_rst_n();
 	// Testbench variables
     reg d;
 	reg enable;
+	reg reset_n;
 	wire q;
     wire q_not;
     
 	// Instantiate the DUT
-	D_latch DL0(
+	d_latch_rst_n DL0(
         .d(d),
 	    .enable(enable),
+		.reset_n(reset_n),
 	    .q(q),
         .q_not(q_not)
 	);
@@ -19,17 +21,17 @@ module tb_D_latch();
     initial begin
 	    $monitor ($time, " enable = %b, d = %b, q = %b, q_not = %b",
 		                   enable, d, q, q_not);
-        enable = 0;
+        enable = 0; reset_n = 0;
 		#2  ; d = 0;
 	    #0.5; d = 1;
 		#1  ; d = 0;
 		#1  ; d = 1;
 		#1.5; enable = 1;
-		#0.1; d = 1;
-		#0.2; d = 0;
+		#0.1; d = 1; 
+		#0.2; d = 0; reset_n = 1;
 		#0.3; d = 1;
 		#1  ; enable = 0; d = 0;
-		#1  ; d = 1;
+		#1  ; d = 1; reset_n = 0;
 		#2  ; d = 0;
 	end
 	
@@ -38,17 +40,3 @@ module tb_D_latch();
         #40 $finish;
     end
 endmodule
-
-/* Output
-0  enable = 0, d = x, q = x, q_not = x
-1  enable = 0, d = 0, q = x, q_not = x
-2  enable = 0, d = 1, q = x, q_not = x
-4  enable = 1, d = 1, q = 1, q_not = 0
-4  enable = 1, d = 0, q = 0, q_not = 1
-4  enable = 1, d = 1, q = 1, q_not = 0
-5  enable = 0, d = 0, q = 1, q_not = 0
-6  enable = 1, d = 1, q = 1, q_not = 0
-8  enable = 1, d = 0, q = 0, q_not = 1
-9  enable = 0, d = 0, q = 0, q_not = 1
-10 enable = 0, d = 1, q = 0, q_not = 1
-*/
