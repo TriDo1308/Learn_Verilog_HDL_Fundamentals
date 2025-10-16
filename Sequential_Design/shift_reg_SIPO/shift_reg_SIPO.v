5294c84e9ca819280a24bb1dc18de1596fd3402c
Cauchy <caotricg1308@gmail.com>
2025-10-16 21:30:16 +0700

    Add shift reg SIPO

--- Sequential_Design/shift_reg_SIPO/shift_reg_SIPO.v
+++ Sequential_Design/shift_reg_SIPO/shift_reg_SIPO.v
@@ -1,17 +1,16 @@
-module shift_reg_pipo(
+module shift_reg_sipo(
 	input reset_n,
 	input clk,
-    input [3:0] d,
+    input sdi, 				// serial data in
 	output reg [3:0] q
     );
 	
 	// Async negative reset_n is used
 	// The input data is the same as the output data
-	// This can be used as a "pipeline register"
 	always @(posedge clk or negedge reset_n) begin
 	    if (!reset_n)
 		    q <= 4'b0;
 	    else
-		    q[3:0] <= d[3:0];  
+		    q[3:0] <= {q[2:0], sdi};
 	end
 endmodule
