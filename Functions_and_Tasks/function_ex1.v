78389328c45bb6e56f71bc8df0ff620a7414032e
Cauchy <caotricg1308@gmail.com>
2025-10-22 20:49:33 +0700

    Add function example 2

--- Functions_and_Tasks/function_ex1.v
+++ Functions_and_Tasks/function_ex1.v
@@ -2,8 +2,7 @@
 
 module func_ex1 ();
     // This function returns the sum of 2 x 8bit numbers
-	// An internal variable with the same name as the function is
-	// created inside it and is used for returning the value
+	// An internal variable with the same name as the function is created inside it and is used for returning the value
 	function [8:0] sum (input [7:0] a, input [7:0] b);
 	    begin
 		    sum = a + b;
@@ -15,9 +14,9 @@ module func_ex1 ();
 	
 	initial begin
 	    $monitor ($time, " a = %d, b = %d, sum = %d", a, b, sum(a,b));
-		#1 a = 1  ; b = 9;
-		#1 a = 13 ; b = 66;
-		#1 a = 255; b = 1;
+		#1 a =   1; b =   9;
+		#1 a =  13; b =  66;
+		#1 a = 255; b =   1;
 		#1 a = 255; b = 255;
 	end
 endmodule
